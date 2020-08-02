ac9e100f24eb4d7d98f38b58471bba7c8c237eac
jzhu118 <58021801+jzhu118@users.noreply.github.com>
2020-08-01 20:59:18 -0700

    Updates

---
+++ precache-manifest.fc6d647e1812149ec3e518293d2cb07c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c94ae85cd8cd0fb1a0e3407e4608085",
+    "url": "/covidapp/index.html"
+  },
+  {
+    "revision": "d366e540d5042d265674",
+    "url": "/covidapp/static/css/main.c341e75c.chunk.css"
+  },
+  {
+    "revision": "fc91dfa7a7ff89c9edd2",
+    "url": "/covidapp/static/js/2.8df1e9cd.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/covidapp/static/js/2.8df1e9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d366e540d5042d265674",
+    "url": "/covidapp/static/js/main.b3e392f4.chunk.js"
+  },
+  {
+    "revision": "2c9438d00b33ccd43645",
+    "url": "/covidapp/static/js/runtime-main.2e02ef70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covidapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
